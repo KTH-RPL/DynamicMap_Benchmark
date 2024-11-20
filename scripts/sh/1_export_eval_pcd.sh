59868ebd039d579fd3ae75b71c575ee6c7913979
Kin <kinzhangglimmer@gmail.com>
2024-11-20 12:01:18 +0100

    refactor(deflow): update deflow code to the latest.
    * faster, easier setup, better results for updated weight.
    * eval script auto make also.
    * cmakelists.txt add comment force set python executable.

--- scripts/sh/1_export_eval_pcd.sh
+++ scripts/sh/1_export_eval_pcd.sh
@@ -1,13 +1,17 @@
-data_folder=/home/kin
-benchmarks_path=/home/kin/DynamicMap_Benchmark/methods
+data_folder=/home/kin/DATA_HDD/Dynamic_Papers_assets/Benchmark_data
+benchmarks_code_path=/home/kin/code_online/DynamicMap_Benchmark
 # ====> export output for eval
 min_dis=0.05 # since all voxel-based methods resolution is 0.1 / 2 = 0.05
 for seq_num in 00 05 semindoor av
 do
     for method_name in removert erasor octomap octomapf octomapfg
     do
-        data_path=${data_folder}/data/${seq_num}
-        ${benchmarks_path}/benchmarks/build/export_eval_pcd ${data_path} ${method_name}_output.pcd ${min_dis}
+        data_path=${data_folder}/${seq_num}
+        # if build file not exist, build it
+        if [ ! -f ${benchmarks_code_path}/scripts/build/export_eval_pcd ]; then
+            cd ${benchmarks_code_path}/scripts && cmake -B build && cmake --build build && cd ${benchmarks_code_path}
+        fi
+        ${benchmarks_code_path}/scripts/build/export_eval_pcd ${data_path} ${method_name}_output.pcd ${min_dis}
         echo "exported ${method_name} seq ${seq_num} check ${method_name}_exportGT.pcd"
     done
 done
